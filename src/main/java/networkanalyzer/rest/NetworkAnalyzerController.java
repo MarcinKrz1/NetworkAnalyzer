18445c23d077e8367087af511109915e454341ae
MarcinKrz1 <marcinkrzeminski12@gmail.com>
2018-11-10 19:10:59 +0100

    refactor NetworkAnalyzerController

--- src/main/java/networkanalyzer/rest/NetworkAnalyzerController.java
+++ src/main/java/networkanalyzer/rest/NetworkAnalyzerController.java
@@ -5,10 +5,7 @@ import networkanalyzer.logic.CalculatorPaths;
 import networkanalyzer.logic.Connection;
 import networkanalyzer.logic.Node;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,29 +15,29 @@ import java.util.List;
 @RequestMapping("")
 public class NetworkAnalyzerController {
 
-    CalculatorPaths calculatorPaths = new CalculatorPaths();
+    private final CalculatorPaths calculatorPaths = new CalculatorPaths();
 
-    @RequestMapping(method = RequestMethod.POST, value = "/nodes")
+    @PostMapping("/nodes")
     public ResponseEntity<List<Node>> postNodes(@Valid @RequestBody List<Node> nodes) {
         calculatorPaths.setNodes(nodes);
         log.info("Created {}", nodes.toString());
         return ResponseEntity.ok(nodes);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/connections")
+    @PostMapping("/connections")
     public ResponseEntity<List<Connection>> putConnections(@RequestBody List<Connection> connections) {
         calculatorPaths.setConnections(connections);
         log.info("Created {}", connections.toString());
         return ResponseEntity.ok(connections);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/bfs")
-    public ResponseEntity<List<Node>> Solution(){
+    @GetMapping("/bfs")
+    public ResponseEntity<List<Node>> Solution() {
         return ResponseEntity.ok(calculatorPaths.BFS_path());
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/greedy")
-    public ResponseEntity<List<Node>> Solution2(){
+    @GetMapping("/greedy")
+    public ResponseEntity<List<Node>> Solution2() {
         return ResponseEntity.ok(calculatorPaths.Greedy_algorithm());
     }
 }
