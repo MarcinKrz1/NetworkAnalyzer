b2bb8be99140a8a3170f696769b2d00cf3d6d211
eufrat <lukasz.zywanski@student.put.poznan.pl>
2018-12-15 19:00:26 +0100

    add init to testBFS

--- src/main/java/networkanalyzer/test/juint/BfsSearchTest.java
+++ src/main/java/networkanalyzer/test/juint/BfsSearchTest.java
@@ -1,7 +1,32 @@
 package networkanalyzer.test.juint;
 
-import static org.junit.Assert.*;
+import networkanalyzer.logic.Connection;
+import networkanalyzer.logic.Node;
+import networkanalyzer.logic.algorithms.BfsSearch;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
 
 public class BfsSearchTest {
+    BfsSearch bs;
+    static List<Node> nodes;
+    static List<Connection>connections;
+
+    @BeforeClass
+    public static void init(){
+        nodes = new ArrayList<Node>();
+        connections = new ArrayList<Connection>();
+    }
+
+    @Test
+    public void test1(){
+        assertTrue(true);
+    }
 
 }
